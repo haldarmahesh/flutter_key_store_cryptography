70e499dc8fc7863bab6d6751848556902fa1aa36
Mahesh Haldar <haldar.mahesh@gmail.com>
2020-05-03 01:56:57 +0800

    Fix lock

--- android/src/main/kotlin/com/dkatalis/cipher/CallMethodTypeEnum.kt
+++
@@ -1,7 +0,0 @@
-package com.plugin.flutter.cryptography.key_store
-
-enum class CallMethodTypeEnum {
-    getPublicKey,
-    encrypt,
-    verify
-}
--- android/src/main/kotlin/com/dkatalis/cipher/VerifyArgumentsMapKeysEnum.kt
+++
@@ -1,6 +0,0 @@
-package com.plugin.flutter.cryptography.key_store
-
-enum class VerifyArgumentsMapKeysEnum {
-    plainText,
-    signature
-}
